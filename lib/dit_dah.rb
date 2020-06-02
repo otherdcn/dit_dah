2b28a9bd71f8090710afb3d9ae1aa4cbb19fa3be
OtherDCN <danny.c.ntwari@gmail.com>
2020-06-02 15:56:45 +0200

    Add setters and getters for input_text
    Add setter and getter methods for instance variable input_text from DitDah::DitDah class.

--- lib/dit_dah.rb
+++ lib/dit_dah.rb
@@ -4,7 +4,16 @@ module DitDah
   class Error < StandardError; end
   
   class DitDah
-    def initialize()
+    def initialize(input_text=nil)
+      @input_text = input_text
+    end
+
+    def get_input_text
+      @input_text
+    end
+
+    def set_input_text(input_text)
+      @input_text = input_text
     end
   end
 end
--- spec/dit_dah_spec.rb
+++ spec/dit_dah_spec.rb
@@ -13,5 +13,46 @@ RSpec.describe DitDah do
     it "exist and can be instantiated" do
       expect(DitDah::DitDah.new).to_not be_nil
     end
+
+    context "initializing instance variables" do
+      describe "#get_input_text" do
+        context "when getting input_text from an uninitialized object" do
+          it "returns nil" do
+            obj = DitDah::DitDah.new()
+            
+            expect(obj.get_input_text).to be_nil
+          end
+        end
+
+        context "when getting input_text from an initialized object with string 'placeholder'" do
+          it "returns 'placeholder'" do
+            obj = DitDah::DitDah.new("placeholder")
+
+            expect(obj.get_input_text).to eq("placeholder")
+          end
+        end
+      end
+
+      describe "#set_input_text" do
+        context "when setting the value 'fragilistic' to input_text" do
+          it "will be successfully set if #get_input_text equals to 'fragilistic'" do
+            obj = DitDah::DitDah.new
+
+            obj.set_input_text("fragilistic")
+
+            expect(obj.get_input_text).to eq("fragilistic")
+          end
+
+          it "will successfully overwrite existing values in 'input_text'" do
+            obj = DitDah::DitDah.new("super")
+
+            obj.set_input_text("fragilistic")
+
+            expect(obj.get_input_text).to eq("fragilistic")
+            expect(obj.get_input_text).to_not eq("super")
+          end
+        end
+      end
+    end
   end
 end
